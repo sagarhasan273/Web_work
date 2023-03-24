4077859c8dfb36e52d0a3cbd9e6428f1082a5314
sagarhasan273 <sagarhasan273@gmail.com>
2023-03-24 22:36:29 +0600

    div create

---
+++ create multiple div/index.js
@@ -0,0 +1,6 @@
+function createDiv() {
+    var div = document.querySelector(".container");
+    const element = document.createElement('div');
+    element.classList.add('box');
+    div.appendChild(element);
+}
